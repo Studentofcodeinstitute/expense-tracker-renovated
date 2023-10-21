467d5015727daad8667c7d2212486d28e351d616
Studentofcodeinstitute <>
2023-10-21 11:57:41 +0000

    Add create onclick button to delete single line function and create function to get store current value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 let row = 1;
+const deleteHTML = `<button onclick="onClick(this);">Delete</button>`;
+
+function getCurrentLocalStorageValue() {
+    let currentData = localStorage.getItem("data");
+    try {
+        currentData = JSON.parse(currentData) || [];
+    } catch {
+        currentData = [];
+    }
+
+    return currentData;
+}
